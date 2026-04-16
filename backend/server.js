dbef053d00ad664bf69ce465d3cdc5fe56cedbc4
csakegyfiua_vilag_ban_dik <magyari.david.krisztian@ganziskola.hu>
2026-04-17 00:14:18 +0100

    veglegesteszt

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,11 @@ const forumRoutes = require('./routes/forum');
 const jelvenyRoutes = require('./routes/jelvenyek');
 const galeriaRoutes = require('./routes/galeria');
 
-// Statikus mappa kiszolgálása a képekhez (ha az 'uploads' mappába mented őket)
+
 app.use('/uploads', express.static('uploads')); 
 
 // útvonalak regisztrációja
-// MEGJEGYZÉS: A kedvencek funkciókat a turak.js kezeli az /api/turak/kedvencek úton
+
 app.use('/api/turak', turaRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/forum', forumRoutes);
@@ -35,9 +35,11 @@ app.get('/', (req, res) => {
     res.send('<h1>A Vándor Bakancs szerver online!</h1>');
 });
 
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`\n🚀 Szerver fut: http://localhost:${PORT}`);
+    console.log(`\n🚀 Szerver fut a ${PORT}-es porton!`);
     console.log(`🔗 Túra API teszt: http://localhost:${PORT}/api/turak`);
     console.log(`🔗 Fórum API teszt: http://localhost:${PORT}/api/forum/temak`);
     console.log(`🔗 Galéria API teszt: http://localhost:${PORT}/api/galeria`); 
